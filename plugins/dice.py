593e445b58ba67986b20f04895c69eb1ed553319
Ledenel <ledenelintelli@gmail.com>
2021-01-17 00:17:25 +0000

    change to nonebot logger

--- plugins/dice.py
+++ plugins/dice.py
@@ -1,22 +1,22 @@
 from nonebot import on_command, CommandSession
 import asyncio
-import logging
+from nonebot.log import logger
 
 @on_command('d', aliases=('dice', '骰', 'roll'))
 async def diceroll(session: CommandSession):
     # get real command content
     command_text = session.current_arg_text.strip()
     cmd = f"roll {command_text}"
-    logging.info(f"trying to execute {cmd}")
+    logger.info(f"trying to execute {cmd}")
     proc = await asyncio.create_subprocess_shell(
         cmd,
         stdout=asyncio.subprocess.PIPE,
         stderr=asyncio.subprocess.PIPE
     )
-    logging.info(f"{cmd} process created.")
+    logger.debug(f"{cmd} process created.")
     stdout, stderr = await proc.communicate()
-    logging.info(f"got stdout \nf{stdout}")
-    logging.info(f"got stderr \nf{stderr}")
+    logger.debug(f"got stdout \nf{stdout}")
+    logger.debug(f"got stderr \nf{stderr}")
 
     # also can use proc.returncode
     await session.send(stdout.decode())
