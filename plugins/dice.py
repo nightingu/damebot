f237cd7ac0a1c722d2c06345939af94faac4e855
Ledenel <ledenelintelli@gmail.com>
2021-01-17 00:12:00 +0000

    add logging to trace

--- plugins/dice.py
+++ plugins/dice.py
@@ -1,16 +1,22 @@
 from nonebot import on_command, CommandSession
 import asyncio
+import logging
 
 @on_command('d', aliases=('dice', '骰', 'roll'))
 async def diceroll(session: CommandSession):
     # get real command content
     command_text = session.current_arg_text.strip()
     cmd = f"roll {command_text}"
+    logging.info(f"trying to execute {cmd}")
     proc = await asyncio.create_subprocess_shell(
         cmd,
         stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE)
-
+        stderr=asyncio.subprocess.PIPE
+    )
+    logging.info(f"{cmd} process created.")
     stdout, stderr = await proc.communicate()
+    logging.info(f"got stdout \nf{stdout}")
+    logging.info(f"got stderr \nf{stderr}")
+
     # also can use proc.returncode
     await session.send(stdout.decode())
