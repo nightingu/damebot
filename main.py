436cc88d8fa37937a6c01aa606df4b147b9b00c6
Ledenel <ledenelintelli@gmail.com>
2021-01-21 16:11:10 +0000

    try to add middleware to migiate cors error

--- main.py
+++ main.py
@@ -2,11 +2,25 @@ import nonebot
 from nonebot import drivers
 drivers.BaseWebSocket = drivers.WebSocket
 from os import path
-
+from fastapi.middleware.cors import CORSMiddleware
 from nonebot.adapters.cqhttp import Bot as CQHTTPBot
 
 nonebot.init(command_start = {',', '，'})
 driver = nonebot.get_driver()
+app = nonebot.get_app()
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 driver.register_adapter("cqhttp", CQHTTPBot)
 nonebot.load_builtin_plugins()
 nonebot.load_plugin("nonebot_plugin_test")
