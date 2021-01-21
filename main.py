20419d57972211318bb401aedb86f29a3bd57ac7
Ledenel <ledenelintelli@gmail.com>
2021-01-21 13:55:28 +0000

    monkeypatching plugin test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import nonebot
-
+from nonebot import drivers
+drivers.BaseWebSocket = drivers.WebSocket
 from os import path
 
 from nonebot.adapters.cqhttp import Bot as CQHTTPBot
@@ -8,6 +9,7 @@ nonebot.init(command_start = {',', '，'})
 driver = nonebot.get_driver()
 driver.register_adapter("cqhttp", CQHTTPBot)
 nonebot.load_builtin_plugins()
+nonebot.load_plugin("nonebot_plugin_test")
 
 if __name__ == '__main__':
     nonebot.run(host='127.0.0.1', port=8081)
