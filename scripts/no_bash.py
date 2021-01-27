2116ec697348e2da644d0a81ec4aeb4f6a2a46ff
Ledenel <ledenelintelli@gmail.com>
2021-01-27 19:51:43 +0800

    add options first to recogize rest

--- scripts/no_bash.py
+++ scripts/no_bash.py
@@ -18,7 +18,7 @@ from docopt import docopt
 import subprocess
 
 if __name__ == '__main__':
-    arguments = docopt(__doc__, version='No-bash 0.0.1')
+    arguments = docopt(__doc__, version='No-bash 0.0.1', options_first=True)
     if arguments["<command>"]:
         cmd = [arguments["<command>"]] + arguments["ARGS"]
         print(f"executing {cmd}", file=sys.stderr)
