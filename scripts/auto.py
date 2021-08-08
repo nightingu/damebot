99e4bb1e948063bb928ae3dd4148a84eca0e48e9
Ledenel <ledenelintelli@gmail.com>
2021-08-08 17:10:37 +0000

    add module system design, spacy

--- scripts/auto.py
+++ scripts/auto.py
@@ -1,11 +1,17 @@
 #!/usr/local/bin/python
 
-"""用于控制自动回复，生成自动回复用命令，以及管理自动回复模板，自动回复条件。
+properties = "type|extract|template|survival|period"
+properties_actions = "clear|set|add"
+module_actions = "create|status|remove|on|off"
+
+f"""用于控制自动回复，生成自动回复用命令，以及管理自动回复模板，自动回复条件。
 你可以在其中添加支持的其他命令来使得机器人自动工作。
 
 Usage:
-  auto on
-  auto off
+  auto ({module_actions}) all [permanent]
+  auto ({module_actions}) <module> [permanent]
+  auto ({properties_actions}) ({properties}) all <args>...
+  auto ({properties_actions}) ({properties}) <module> <args>...
   auto gen <text>
 
 Options:
@@ -18,6 +24,31 @@ import requests
 import os
 import re
 import random
+
+from json_store import JSONStore
+
+from datetime import datetime, timedelta
+
+class WhooshQueryModule:
+    def __init__(self, name):
+        self.name = name
+        self.properties = JSONStore(f"{name}")
+        self.properties.setdefault("type", "whoosh")
+        self.properties.setdefault("extract", "type:subtree NOT pos:PUNCT text_len:7")
+        self.properties.setdefault("template", "pj on '' '{}' ''")
+        self.properties.setdefault("survival", 1)
+        self.properties.setdefault("period", timedelta(hours=0.5))
+
+    
+    def __getitem__(self, name):
+        return self.properties[name]
+    
+    def __setitem__(self, name, value):
+        self.properties[name] = value
+        self.properties.sync()
+
+
+
 number_pattern = re.compile("([0-9]+)(\\-([0-9]+))?(\\-([0-9]+))?")
 
 from itertools import zip_longest
