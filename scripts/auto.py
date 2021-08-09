f961adeae1d7d564d59be09a9cd9d9bf27fabeb4
Ledenel <ledenelintelli@gmail.com>
2021-08-09 10:18:23 +0000

    implemented extract template gen command

--- scripts/auto.py
+++ scripts/auto.py
@@ -6,6 +6,13 @@ module_actions = "create|status|remove|on|off"
 
 f"""用于控制自动回复，生成自动回复用命令，以及管理自动回复模板，自动回复条件。
 你可以在其中添加支持的其他命令来使得机器人自动工作。
+每个模组可以控制开关(on/off)、设置自然语言提取和命令生成模板(extract/template)以及设置触发频率(survival/period)。
+开关默认会持续该模组period的时间。如果使用permanent选项则会永久开关。
+
+自然语言提取的命令格式请参考https://whoosh.readthedocs.io/en/latest/querylang.html
+你可以使用nlp inspect <text>命令来查看支持的短语提取字段。它会默认执行type:subtree token_len:[2 TO] contains_punct:no命令来搜索两个及以上的词组成的短语（不包括标点符号）。
+使用nlp test <text> <query>命令来尝试提取短语。
+
 
 Usage:
   auto ({module_actions}) all [permanent]
@@ -13,6 +20,8 @@ Usage:
   auto ({properties_actions}) ({properties}) all <args>...
   auto ({properties_actions}) ({properties}) <module> <args>...
   auto gen <text>
+  auto nlp inspect <text>
+  auto nlp test <text> <args>...
 
 Options:
   -h --help     Show this screen.
@@ -56,11 +65,29 @@ class WhooshQueryModule:
     def __getitem__(self, name):
         return self.properties[name]
     
+    def extract(self, text, text_only="no"):
+        return requests.get(
+            'http://spacy:5000/', params={
+                "query": "type:subtree token_len:[2 TO] contains_punct:no",
+                "text_only": text_only,
+                "text": text,
+                }).json()
+    
+    def template(self, extracted_data):
+        fmt = self["template"]
+        template_format_count = fmt.count('{') - fmt.count('{{') * 2
+        items = random.sample(extracted_data, k=template_format_count)
+        if not isinstance(items[0], str):
+            if "text" in items[0]:
+                items = [t.text for t in items]
+        return fmt.format(*items)
+
+    def gen(self, text):
+        return self.template(self.extract(text, text_only="yes"))
+
     def __setitem__(self, name, value):
         self.properties[name] = value
         self.properties.sync()
-    
-
 
 
 number_pattern = re.compile("([0-9]+)(\\-([0-9]+))?(\\-([0-9]+))?")
@@ -69,6 +96,7 @@ from itertools import zip_longest
 import glob
 if __name__ == '__main__':
     arguments = docopt(__doc__, version='auto 0.0.1 自动命令生成', options_first=True)
+    os.makedirs("module", exist_ok=True)
     if arguments["on"]:
         with open('auto-mode.touch', "w") as f:
             pass
